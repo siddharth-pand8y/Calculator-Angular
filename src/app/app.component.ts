5e8234909b7f4a07cef8f158126a84d1024bc6dd
Siddharth <siddharth.pand8y@gmail.com>
2020-03-05 23:59:01 +0530

    HTML: Iteration #2

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,5 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
-
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
@@ -8,7 +7,15 @@ import { Router } from '@angular/router';
 })
 export class AppComponent {
   title = 'Calculator-Angular';
+  currentTab = '';
   constructor(private router: Router) {
     this.router.navigateByUrl('/calculator');
+    console.log(this.router.url);
+    this.currentTab =
+      this.router.url === '/converter'
+        ? 'Converter'
+        : this.router.url === '/calculator'
+        ? 'Calculator'
+        : 'this.router.url';
   }
 }
