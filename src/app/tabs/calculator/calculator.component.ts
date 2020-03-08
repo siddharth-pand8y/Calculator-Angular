365038af6cccf3c6b75484d694e1e823869755b1
Siddharth <siddharth.pand8y@gmail.com>
2020-03-08 23:50:54 +0530

    TS: Calculation Implementation

--- src/app/tabs/calculator/calculator.component.ts
+++ src/app/tabs/calculator/calculator.component.ts
@@ -6,10 +6,79 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./calculator.component.scss']
 })
 export class CalculatorComponent implements OnInit {
+  output = 0;
+  previousInputisOperator = false;
+  calculationInput = '';
+  openBracket = 0;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+
+
+  handleNumpadPress(value) {
+    this.previousInputisOperator = false;
+    this.calculationInput += value;
+    console.log('Operand', this.calculationInput);
   }
 
+  handleOperatorPress(value) {
+    if (this.calculationInput.length === 0) {
+      console.warn('Operator entered before operand');
+    } else {
+      if (this.previousInputisOperator === true) {
+        switch (value) {
+          case '/':
+          case '*':
+          case '+':
+          case '-':
+          case '%':
+            this.calculationInput = this.calculationInput.slice(0, -1) + value;
+            break;
+          default:
+            console.warn('Switch Default', value);
+            break;
+        }
+        console.log('Operator #1', this.calculationInput);
+      } else {
+        this.calculationInput += value;
+        console.log('Operator #2', value);
+      }
+      this.previousInputisOperator = true;
+    }
+  }
+
+  brackets() {
+    if (this.openBracket < 1) {
+      this.calculationInput += '(';
+      this.openBracket++;
+    } else {
+      this.calculationInput += ')';
+      this.openBracket--;
+    }
+  }
+
+  makeValueNegative() {
+    this.calculationInput += '(-';
+    this.openBracket++;
+  }
+
+  clear() {
+    this.calculationInput = '';
+    this.output = 0;
+     console.log(this.output, this.calculationInput);
+  }
+
+  Delete() {
+    this.calculationInput = this.calculationInput.substring(
+      0,
+      this.calculationInput.length - 1
+    );
+  }
+
+  calculate() {
+    this.output = eval(this.calculationInput);
+    console.log('Output', this.output);
+  }
 }
