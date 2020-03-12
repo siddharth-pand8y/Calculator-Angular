19123a07a661cfb5a612f172e108eeeb78456213
Siddharth <siddharth.pand8y@gmail.com>
2020-03-12 23:51:36 +0530

    HTML: Iteartion #6

--- src/app/tabs/converter/converter.component.ts
+++ src/app/tabs/converter/converter.component.ts
@@ -1,15 +1,70 @@
 import { Component, OnInit } from '@angular/core';
-
+import { FormControl } from '@angular/forms';
 @Component({
   selector: 'app-converter',
   templateUrl: './converter.component.html',
   styleUrls: ['./converter.component.scss']
 })
 export class ConverterComponent implements OnInit {
+  conversionType = new FormControl('Area');
 
-  constructor() { }
+  units = {
+    area: [
+      {
+        name: 'Square Centimetre',
+        symbol: 'cm\u00B2',
+        base_unit: 0.0001
+      },
+      {
+        name: 'Hectare',
+        symbol: 'ha',
+        base_unit: 10000
+      },
+      {
+        name: 'Square Kilometre',
+        symbol: 'km\u00B2',
+        base_unit: 1000000
+      },
+      {
+        name: 'Square Metres',
+        symbol: 'm\u00B2',
+        base_unit: 1
+      }
+    ],
+    Length: [
+      {
+        name: 'Kilometre',
+        symbol: 'km',
+        base_unit: 1000
+      },
+      {
+        name: 'centimetre',
+        symbol: 'cm',
+        base_unit: 0.01
+      },
+      {
+        name: 'millimetre',
+        symbol: 'mm',
+        base_unit: 0.000001
+      },
+      {
+        name: 'nanometre',
+        symbol: 'nm',
+        base_unit: 0.000000001
+      },
+      {
+        name: 'Metre',
+        symbol: 'm',
+        base_unit: 1
+      },
+    ]
+  };
 
-  ngOnInit(): void {
+  constructor() {
+    this.conversionType.valueChanges.subscribe((value) => {
+      console.log(value);
+    });
   }
 
+  ngOnInit(): void {}
 }
--- src/app/tabs/tabs.module.ts
+++ src/app/tabs/tabs.module.ts
@@ -2,13 +2,18 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { CalculatorComponent } from './calculator/calculator.component';
 import { ConverterComponent } from './converter/converter.component';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
+import { AppModule } from '../app.module';
 
 
 
 @NgModule({
   declarations: [CalculatorComponent, ConverterComponent],
   imports: [
-    CommonModule
+    AppModule,
+    CommonModule,
+    FormsModule,
+    ReactiveFormsModule
   ]
 })
 export class TabsModule { }
